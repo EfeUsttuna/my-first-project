82bbbc707ed54be3ba7ca40f322052ac9b7dff97
efeus <efeus@USTTUNA-PC>
2023-12-23 19:11:50 +0300

    devolopment 3

--- My First Project/My First Project/Program.cs
+++ My First Project/My First Project/Program.cs
@@ -5,4 +5,13 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!");
+
 
